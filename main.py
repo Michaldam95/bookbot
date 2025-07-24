b705a7ae0bb65b3deb9b3dcb57fb8d45f94f4a0d
Michal Damek <michal.damek71@gmail.com>
2025-07-24 21:46:55 +0200

    Final commit of bookbot

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("greetings boots")
+import stats
+import sys
+
+
+def get_book_text(filepath: str) -> str:
+    with open(filepath) as f:
+        return f.read()
+    
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path_to_book = sys.argv[1]
+    content = get_book_text(path_to_book)
+    word_co = stats.word_count(content)
+    letter_co = stats.letter_count(content)
+    stats.print_report(word_co, letter_co, path_to_book)
+
+main()
